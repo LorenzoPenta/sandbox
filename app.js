bf159da8d7b1bff51723883c20ed45cdf811eb73
lorenzopenta <lorenzopenta@outlook.com>
2020-04-07 18:39:47 +0200

    mumbo jumbo

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let name = "Lorenzo Penta";
-let address, zip, state;
+// let name = "Lorenzo Penta";
+// let address, zip, state;
 
-address = "101 main street";
-zip = "60612"
-state = "CA"
+// address = "101 main street";
+// zip = "60612"
+// state = "CA"
 
-console.log(name,address);
+// console.log(name,address);
